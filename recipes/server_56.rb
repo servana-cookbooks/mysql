ae2877336383502ed80d9f01e70f2be488980de8
Tass Skoudros <tass@skystack.com>
2013-07-08 23:03:42 +0100

    updated to use original mysql as a base then remove it and install the custom one
    just need to add the mysql upgrade
    
    Signed-off-by: Tass Skoudros <tass@skystack.com>

--- recipes/server_56.rb
+++ recipes/server_56.rb
@@ -1,5 +1,16 @@
 
-::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
+
+
+node.set['mysql']['data_dir'] = "/opt/mysql/data" 
+include_recipe "mysql::server"
+
+
+node['mysql']['server']['packages'].each do |package_name|
+  package package_name do
+    action :remove
+  end
+end
+
 include_recipe "mysql::client"
 
 package "libaio-dev" do 
@@ -7,12 +18,12 @@ package "libaio-dev" do
 end
 
 case node['kernel']['machine']
-  when "x86_64"
-      get_mysql =  "mysql-5.6.12-debian6.0-x86_64.deb"
-  when "i686"
-      get_mysql =  "mysql-5.6.12-debian6.0-i686.deb"
+when "x86_64"
+    get_mysql =  "mysql-5.6.12-debian6.0-x86_64.deb"
+when "i686"
+    get_mysql =  "mysql-5.6.12-debian6.0-i686.deb"
 else
-      get_mysql =  "mysql-5.6.12-debian6.0-i686.deb"
+    get_mysql =  "mysql-5.6.12-debian6.0-i686.deb"
 end
 
 execute  "wget -O /tmp/#{get_mysql} http://dev.mysql.com/get/Downloads/MySQL-5.6/#{get_mysql}/from/http://cdn.mysql.com/"
@@ -25,53 +36,20 @@ end
 
 execute "chown -R mysql:mysql /var/run/mysqld"
 
-group "mysql" do
-  system true
-  action :create
-end
+#group "mysql" do
+#  system true
+#  action :create
+#end
 
-user "mysql" do
-  gid "mysql"
-  shell "/bin/bash"
-  supports :manage_home => false
-  system true
-  action :create
-end
+#user "mysql" do
+#  gid "mysql"
+#  shell "/bin/bash"
+#  supports :manage_home => false
+#  system true
+#  action :create
+#end
 
-node.set_unless['mysql']['server_debian_password'] = secure_password
-node.set_unless['mysql']['server_root_password']   = secure_password
-node.set_unless['mysql']['server_repl_password']   = secure_password
-
-if platform?(%w{debian ubuntu})
-
-  directory "/var/cache/local/preseeding" do
-    owner "root"
-    group node['mysql']['root_group']
-    mode 0755
-    recursive true
-  end
-
-  execute "preseed mysql-server" do
-    command "debconf-set-selections /var/cache/local/preseeding/mysql-server.seed"
-    action :nothing
-  end
-
-  template "/var/cache/local/preseeding/mysql-server.seed" do
-    source "mysql-server.seed.erb"
-    owner "root"
-    group node['mysql']['root_group']
-    mode "0600"
-    notifies :run, resources(:execute => "preseed mysql-server"), :immediately
-  end
-
-  template "#{node['mysql']['conf_dir']}/debian.cnf" do
-    source "debian.cnf.erb"
-    owner "root"
-    group node['mysql']['root_group']
-    mode "0600"
-  end
-
-end
+node.set['mysql']['basedir'] = "/opt/mysql/server-5.6" 
 
 dpkg_package "mysql-server" do
   source "/tmp/#{get_mysql}"
@@ -83,7 +61,7 @@ command <<-COMMAND
 (
 cat <<'EOF'
 #!/bin/bash 
-export PATH=/opt/mysql/server-5.6/bin:$PATH
+export PATH=#{node['mysql']['basedir']}/bin:$PATH
 EOF
 ) > /etc/profile.d/mysql.sh
 COMMAND
@@ -98,50 +76,7 @@ link "/etc/my.cnf" do
 	to "/etc/mysql/my.cnf"
 end
 
-unless platform?(%w{debian ubuntu})
-  node.set['mysql']['mysqladmin_bin'] = "/opt/mysql/server-5.6/bin/mysqladmin"
-  execute "assign-root-password" do
-    command "\"#{node['mysql']['mysqladmin_bin']}\" -u root password \"#{node['mysql']['server_root_password']}\""
-    action :run
-    only_if "\"#{node['mysql']['mysql_bin']}\" -u root -e 'show databases;'"
-  end
-
-end
-
- grants_path = node['mysql']['grants_path']
-
-  begin
-    t = resources("template[#{grants_path}]")
-  rescue
-    Chef::Log.info("Could not find previously defined grants.sql resource")
-    t = template grants_path do
-      source "grants.sql.erb"
-      owner "root" unless platform? 'windows'
-      group node['mysql']['root_group'] unless platform? 'windows'
-      mode "0600"
-      action :create
-    end
-  end
-
-   node.set['mysql']['mysql_bin'] = "/opt/mysql/server-5.6/bin/mysql"
-   execute "mysql-install-privileges" do
-      command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"#{grants_path}\""
-      action :nothing
-      subscribes :run, resources("template[#{grants_path}]"), :immediately
-   end
-
-
-
- service "mysql" do
-    service_name node['mysql']['service_name']
-    if node['mysql']['use_upstart']
-      restart_command "restart mysql"
-      stop_command "stop mysql"
-      start_command "start mysql"
-    end
-    supports :status => true, :restart => true, :reload => true
-    action :nothing
-  end
+#execute "#{node['mysql']['basedir']}/bin/mysql_upgrade -uroot -p#{node['mysql']['server_root_password']} --all-databases=true"
  
  skip_federated = case node['platform']
                    when 'fedora', 'ubuntu', 'amazon'
@@ -168,4 +103,4 @@ end
     variables :skip_federated => skip_federated
   end
 
-#mysql_upgrade
+
